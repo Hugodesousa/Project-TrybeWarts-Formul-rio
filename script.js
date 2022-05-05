5114e9af618ecc3d8670ed933058927fb5b7aeec
Marcelo Ataíde <msa.trybe@gmail.com>
2022-05-05 17:32:37 -0300

    Requisito 3 e correçao do lint:styles

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-// Formulario de Login 
+// Formulario de Login
+
+const email = document.getElementById('email');
+const password = document.getElementById('password');
 
 const login = document.querySelector('#login');
-login.addEventListener('click', (event) => {
-event.preventDefault();
-console.log(email.value);
 
+login.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  if (email.value === 'tryber@teste.com' && password.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+  console.log(email.value);
+  console.log(password.value);
 });
