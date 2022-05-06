974fd518ba0870660660ad7395306ff1d9eef4c5
Marcelo Ataíde <msa.trybe@gmail.com>
2022-05-06 10:19:36 -0300

    Requisitos 13, 14, 15, 16, 17, 18, 19(merge)

--- script.js
+++ script.js
@@ -16,3 +16,42 @@ login.addEventListener('click', (event) => {
   console.log(email.value);
   console.log(password.value);
 });
+
+const agreement = document.getElementById('agreement');
+const submitBtn = document.getElementById('submit-btn');
+
+function toggleDisabled(isDisabled) {
+  if (isDisabled) {
+    submitBtn.disabled = true;
+  } else {
+    submitBtn.disabled = false;
+  }
+}
+
+agreement.addEventListener('click', () => {
+  const isDisabled = agreement.checked;
+
+  toggleDisabled(!isDisabled);
+});
+
+toggleDisabled(true);
+
+const textarea = document.getElementById('textarea');
+const counter = document.getElementById('counter');
+// console.log(typeof +counter.innerText);
+// console.log(textarea);
+
+const incrementCounter = Number(counter.innerText);
+
+function characterCounter(value) {
+  value += value;
+  console.log(value);
+}
+
+textarea.addEventListener('keyup', () => {
+  characterCounter(incrementCounter);
+});
+
+submitBtn.addEventListener('click', () => {
+  console.log('Enviou!!!');
+});
